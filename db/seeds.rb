840828a2a0e5b0e2c203672c1ac77e1b936b6c05
Allan <kipropsallan@gmail.com>
2022-09-14 19:31:19 +0300

    modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,23 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+# Create Users
+4.times do 
+    User.create(
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        password: Faker::Alphanumeric.alphanumeric(number: 7, min_alpha: 3, min_numeric: 3),
+    )
+end
+
+
+# Create Recipes
+44.times do
+    Recipe.create(
+        name: Faker::Name.name,
+        requirements: Faker::Requirement.requirements,
+        vegetarian: Faker::Vegetarian.vegetarian,
+end
+
+
 
 puts "✅ Done seeding!"
