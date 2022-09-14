2c373442c9fbf946c2dcc9434ad8c12025b75a05
Allan <kipropsallan@gmail.com>
2022-09-14 18:13:51 +0300

    Modified Routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,38 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+
+  get "/users" do
+    User.all.to_json
   end
+
+  get "/recipes" do
+    Recipe.all.to_json
+  end
+  
+  get "/users/:id" do
+    User.find(params[:id]).to_json
+  end
+
+  post "/users" do
+    user = User.new(params[:user])
+    user.save
+    user.to_json
+  end
+
+  get "/saved_Recipes" do
+    Saved_Recipe.all.to_json:include => :recipe
+  end
+
+  post "/saved_Recipes" do
+    recipe = Recipe.new(params[:recipe])
+    recipe.save
+    recipe.to_json
+  end
+
+  delete "/saved_Recipes" do
+    Saved_Recipes.find(params[:id]).destroy
+  end
+
 
 end
